038b25f50328ee6bfd703bc8dddc408dc6b56dfa
梁碩珩 <mandyshliang@moneydj.com>
2020-02-20 14:21:09 +0800

    [CH1] 資料夾名稱更新

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,4 +1,5 @@
-describe('Appointment', () => {
-    it('renders the customer first name', () => {
-    });
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+
+describe('Appointment', () => {  it('renders the customer first name', () => {  }); });
