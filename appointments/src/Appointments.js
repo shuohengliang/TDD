47ac334bd4f703001421aaafa9b47a77edfe1cd6
梁碩珩 <mandyshliang@moneydj.com>
2020-02-21 17:49:21 +0800

    [CH1] failed test P.42

--- appointments/src/Appointments.js
+++ appointments/src/Appointments.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
 export const Appointment = ({customer}) => <div>{customer.firstName}</div>;
-export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
+export const AppointmentsDayView = () => <div id="appointmentsDayView"><ol /></div>;
