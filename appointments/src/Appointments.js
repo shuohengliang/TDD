ae2f76d2a5d0085aaafbbe10a93bb8ba2559a080
梁碩珩 <mandyshliang@moneydj.com>
2020-02-22 11:01:39 +0800

    [CH1] pass test P.46

--- appointments/src/Appointments.js
+++ appointments/src/Appointments.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+const appointmentTimeOfDay = statrsAt => {
+	const [h, m] = new Date(statrsAt).toTimeString().split(':');
+	return `${h}:${m}`;
+}
+
 export const Appointment = ({customer}) => <div>{customer.firstName}</div>;
 export const AppointmentsDayView = ({appointments}) => (
 	<div id="appointmentsDayView">
 		<ol>
       		{appointments.map(appointments => (
-        		<li key={appointments.startsAt}/>
+        		<li key={appointments.startsAt}>{appointmentTimeOfDay(appointments.startsAt)}</li>
       		))}
     	</ol>
   	</div>
